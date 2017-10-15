76379167e5681cc870e09c0016c1b9cf50525b22
Flzzyf <flzzyf076@gmail.com>
2017-10-15 22:22:54 +0800

    3.0

---
+++ W.G.B.P.O/Assets/EndPoint.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour {
+
+    List<GameObject> targetList = new List<GameObject>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        targetList.Add(collision.gameObject);
+
+        if (targetList.Count == 1)
+        {
+            Debug.Log("减速");
+
+            TimeSlow();
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        targetList.Remove(collision.gameObject);
+
+        if(targetList.Count == 0)
+        {
+            GameSpeedManager.SetTimeScale(1f);
+
+        }
+    }
+
+    void TimeSlow()
+    {
+        GameSpeedManager.SetTimeScale(0.4f);
+
+    }
+}
---
+++ W.G.B.P.O/Assets/Scripts/GameSpeedManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GameSpeedManager : MonoBehaviour {
+
+    private void Update()
+    {
+        //按1加快，2减慢，3恢复正常时间速度
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetTimeScale(Time.timeScale + 1);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetTimeScale(Time.timeScale - 0.1f);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && Time.timeScale > 0)
+        {
+            SetTimeScale(1);
+
+        }
+
+    }
+
+    public static void SetTimeScale(float _amount)
+    {
+        Time.timeScale = _amount;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+
+    }
+
+}
